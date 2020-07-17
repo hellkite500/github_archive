e7f805f2ca0dc01bed885b49128a6cf6f8812a9a
Nels Frazier <nels.frazier@noaa.gov>
2020-07-17 16:49:12 -0500

    test for get_repo_meta

--- github_scrape/test/test_get_github.py
+++ github_scrape/test/test_get_github.py
@@ -34,3 +34,26 @@ class TestGetGithub(unittest.TestCase):
             if p.is_file():
                 p.unlink()
 
+    def test_get_repo_meta(self):
+        """
+            Test the archive_repo function to ensure all meta data is properly captured
+        """
+        meta = get_repo_meta(self.repo, self.time, TestGetGithub._current_dir)
+        self.assertIsNotNone(meta)
+        self.assertTrue(len(meta), 6)
+        #defer name substitution
+        pattern =  "{repo}_{name}_{time}.json".format(repo=self.repo_string, name="{name}", time=self.time)
+        self.assertEqual(meta[0].name, pattern.format(name='comments'))
+        self.assertEqual(meta[1].name, pattern.format(name='issues'))
+        self.assertEqual(meta[2].name, pattern.format(name='issue_comments'))
+        self.assertEqual(meta[3].name, pattern.format(name='pulls'))
+        self.assertEqual(meta[4].name, pattern.format(name='pulls_comments'))
+        self.assertEqual(meta[5].name, pattern.format(name='pulls_review_comments'))
+
+        self.assertTrue((TestGetGithub._current_dir/pattern.format(name='comments')).exists())
+        self.assertTrue((TestGetGithub._current_dir/pattern.format(name='issues')).exists())
+        self.assertTrue((TestGetGithub._current_dir/pattern.format(name='issue_comments')).exists())
+        self.assertTrue((TestGetGithub._current_dir/pattern.format(name='pulls')).exists())
+        self.assertTrue((TestGetGithub._current_dir/pattern.format(name='pulls_comments')).exists())
+        self.assertTrue((TestGetGithub._current_dir/pattern.format(name='pulls_review_comments')).exists())
+
